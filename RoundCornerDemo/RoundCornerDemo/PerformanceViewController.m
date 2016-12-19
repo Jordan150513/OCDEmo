2539329a4f5aa663f75682e98020e59943ab0074
qiaodaniMac <qiaodandan@QiaodabdandeiMac.local>
2016-12-19 11:15:28 +0800

    5种圆角的实现方法

--- RoundCornerDemo/RoundCornerDemo/PerformanceViewController.m
+++ RoundCornerDemo/RoundCornerDemo/PerformanceViewController.m
@@ -25,10 +25,9 @@
     UICollectionViewFlowLayout * layout = [[UICollectionViewFlowLayout alloc] init];
     layout.minimumLineSpacing = 5;
     layout.minimumInteritemSpacing = 5;
-    layout.itemSize = [self getItemSize];
+//    layout.itemSize = [self getItemSize];
+    layout.itemSize = [self getImageSize];
     UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height) collectionViewLayout:layout];
-//    collectionView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
-    
     NSString * kIdentifier = @"collectionView";
     [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:kIdentifier];
     //要求跟切图的遮盖的颜色一致
@@ -43,6 +42,7 @@
 -(CGSize)getImageSize{
     return CGSizeMake(80, 80);
 }
+#pragma mark - ItemSize
 -(CGSize)getItemSize{
     
     CGFloat width = ([self getScreenSize].width-[self getImageSize].width*3)/4+[self getImageSize].width;
@@ -50,7 +50,7 @@
     
     return CGSizeMake(width, height);
 }
-
+#pragma mark - CollectionViewDelegate
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
     return 1;
 }
@@ -65,40 +65,130 @@
     
     return cell;
 }
+#pragma mark - 不同方式设置圆角
 -(void)setContentToCell:(UICollectionViewCell *) cell {
     switch (self.rowForMethod) {
         case 0:
         {
+            //第一种：
             //设置cornerRadius和masksToBounds
             UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"AppIcon60x60"]];
             imageView.layer.cornerRadius = 10.0f;
             imageView.layer.masksToBounds = YES;
-            cell.contentView.autoresizesSubviews = NO;
+//            cell.contentView.autoresizesSubviews = NO;
             imageView.translatesAutoresizingMaskIntoConstraints = NO;
             [cell.contentView addSubview:imageView];
-            NSDictionary *dict = NSDictionaryOfVariableBindings(imageView);
-            NSString *horVLF = @"H:|-[imageView]-|";
-            NSString *verVLF = @"V:|-[imageView]-|";
-            NSArray <NSLayoutConstraint *> *horCons = [NSLayoutConstraint constraintsWithVisualFormat:horVLF options:NSLayoutFormatAlignAllCenterX metrics:nil views:dict];
-            NSArray <NSLayoutConstraint *> *verCons = [NSLayoutConstraint constraintsWithVisualFormat:verVLF options:NSLayoutFormatAlignAllCenterY metrics:nil views:dict];
-            [cell.contentView addConstraints:horCons];
-            [cell.contentView addConstraints:verCons];
+            
+//            NSDictionary *dict = NSDictionaryOfVariableBindings(imageView);
+//            NSString *horVLF = @"H:|-[imageView]-|";
+//            NSString *verVLF = @"V:|-[imageView]-|";
+//            NSArray <NSLayoutConstraint *> *horCons = [NSLayoutConstraint constraintsWithVisualFormat:horVLF options:NSLayoutFormatAlignAllCenterX metrics:nil views:dict];
+//            NSArray <NSLayoutConstraint *> *verCons = [NSLayoutConstraint constraintsWithVisualFormat:verVLF options:NSLayoutFormatAlignAllCenterY metrics:nil views:dict];
+//            [cell.contentView addConstraints:horCons];
+//            [cell.contentView addConstraints:verCons];
         }
             break;
         case 1:
-            
+        {
+            //第二种：
+        //UIBezierPath和Core Graphics在图片上画出一个透明的圆
+            UIImageView *imageViewTwo = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 80, 80)];
+            imageViewTwo.image = [UIImage imageNamed:@"AppIcon60x60"];
+            UIGraphicsBeginImageContextWithOptions(imageViewTwo.bounds.size, NO, [UIScreen mainScreen].scale);
+            [[UIBezierPath bezierPathWithRoundedRect:imageViewTwo.bounds cornerRadius:imageViewTwo.frame.size.width] addClip];
+            [imageViewTwo drawRect:imageViewTwo.bounds];
+            imageViewTwo.image = UIGraphicsGetImageFromCurrentImageContext();
+            //结束画图
+            UIGraphicsEndImageContext();
+            imageViewTwo.translatesAutoresizingMaskIntoConstraints = NO;
+            [cell.contentView addSubview:imageViewTwo];
+
+//            NSDictionary *dict = NSDictionaryOfVariableBindings(imageViewTwo);
+//            NSString *horVLF = @"H:|-[imageViewTwo]-|";
+//            NSString *verVLF = @"V:|-[imageViewTwo]-|";
+//            NSArray <NSLayoutConstraint *> *horCons = [NSLayoutConstraint constraintsWithVisualFormat:horVLF options:NSLayoutFormatAlignAllCenterX metrics:nil views:dict];
+//            NSArray <NSLayoutConstraint *> *verCons = [NSLayoutConstraint constraintsWithVisualFormat:verVLF options:NSLayoutFormatAlignAllCenterY metrics:nil views:dict];
+//            [cell.contentView addConstraints:horCons];
+//            [cell.contentView addConstraints:verCons];
+
+        }
             break;
         case 2:
+        {
+        //第三种：
+            //CAShapeLayer和UIBezierPath设置view.layer.mask
+            UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 80, 80)];
+            imageView.image = [UIImage imageNamed:@"AppIcon60x60"];
             
+//            UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:imageView.bounds byRoundingCorners:UIRectCornerAllCorners cornerRadii:imageView.bounds.size];
+//            
+//            CAShapeLayer *maskLayer = [[CAShapeLayer alloc]init];
+//            //设置大小
+//            maskLayer.frame = imageView.bounds;
+//            //设置图形样子
+//            maskLayer.path = maskPath.CGPath;
+//            imageView.layer.mask = maskLayer;
+            [cell.contentView addSubview:imageView];
+            
+            imageView.translatesAutoresizingMaskIntoConstraints = NO;
+//            NSDictionary * dict = NSDictionaryOfVariableBindings(imageView);
+//            NSString * horVLF = @"H:|-[imageView]-|";
+//            NSString * verVLF = @"V:|-[imageView]-|";
+//            NSArray <NSLayoutConstraint *> *horCons = [NSLayoutConstraint constraintsWithVisualFormat:horVLF options:NSLayoutFormatAlignAllCenterX metrics:nil views:dict];
+//            NSArray <NSLayoutConstraint *> *verCons = [NSLayoutConstraint constraintsWithVisualFormat:verVLF options:NSLayoutFormatAlignAllCenterY metrics:nil views:dict];
+//            [cell.contentView addConstraints:horCons];
+//            [cell.contentView addConstraints:verCons];
+            
+            UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:imageView.frame byRoundingCorners:UIRectCornerAllCorners cornerRadii:imageView.frame.size];
+            
+            CAShapeLayer *maskLayer = [[CAShapeLayer alloc]init];
+            //设置大小
+            maskLayer.frame = imageView.frame;
+            //设置图形样子
+            maskLayer.path = maskPath.CGPath;
+            imageView.layer.mask = maskLayer;
+        }
             break;
         case 3:
+        {
+            //第四种：addsubLayer
+            UIImageView * imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 80, 80)];
+            [imageView setImage:[UIImage imageNamed:@"AppIcon60x60"]];
+            CAShapeLayer * layer = [CAShapeLayer layer];
+            UIBezierPath * path = [UIBezierPath bezierPathWithRect:CGRectMake(0, 0, 80, 80)];
+            UIBezierPath * circlePath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, 80, 80)];
+            [path appendPath:circlePath];
+            [path setUsesEvenOddFillRule:YES];
+            [path addClip];
+            layer.path = path.CGPath;
+//            layer.fillColor = [UIColor whiteColor].CGColor;//改成跟北京色一致的
+            layer.fillColor = [UIColor colorWithRed:240/255.f green:240/255.f blue:240/255.f alpha:1].CGColor;
+            layer.fillRule = kCAFillRuleEvenOdd;
+            [imageView.layer addSublayer:layer];
+            [cell.contentView addSubview:imageView];
             
+//            imageView.translatesAutoresizingMaskIntoConstraints = NO;
+//            NSDictionary * dict = NSDictionaryOfVariableBindings(imageView);
+//            NSString * horVLF = @"H:|-[imageView]-|";
+//            NSString * verVLF = @"V:|-[imageView]-|";
+//            NSArray <NSLayoutConstraint *> *horCons = [NSLayoutConstraint constraintsWithVisualFormat:horVLF options:NSLayoutFormatAlignAllCenterX metrics:nil views:dict];
+//            NSArray <NSLayoutConstraint *> *verCons = [NSLayoutConstraint constraintsWithVisualFormat:verVLF options:NSLayoutFormatAlignAllCenterY metrics:nil views:dict];
+//            [cell.contentView addConstraints:horCons];
+//            [cell.contentView addConstraints:verCons];
+
+        }
             break;
         case 4:
+            //两个imageView遮盖显示,切图配合提供
+        {
+            UIImageView * imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 80, 80)];
+            [imageView setImage:[UIImage imageNamed:@"AppIcon60x60"]];
+            [cell.contentView addSubview:imageView];
             
-            break;
-        case 5:
-            
+            UIImageView * imageViewCover = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 80, 80)];
+            [imageViewCover setImage:[UIImage imageNamed:@"cover@2x"]];
+            [cell.contentView addSubview:imageViewCover];
+        }
             break;
 
         default:
--- RoundCornerDemo/RoundCornerDemo/ViewController.m
+++ RoundCornerDemo/RoundCornerDemo/ViewController.m
@@ -20,19 +20,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    self.title = @"N种设置圆角的方式";
-    NSArray * dataSourceArr = @[@"方式1",@"方式2",@"方式3",@"方式4",@"方式5",@"方式6"];
-    self.dataSourceArr = dataSourceArr;
-    NSArray * descSourceArr = @[@"设置cornerRadius和masksToBounds",@"如何实现的描述",@"如何实现的描述",@"如何实现的描述",@"如何实现的描述",@"如何实现的描述"];
-    self.descSourceArr = descSourceArr;
+    self.title = @"设置圆角的5种方式";
     
-    UITableView * tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-100) style:UITableViewStylePlain];
+    [self setDefaultData];
+    [self createUI];
+}
+-(void)setDefaultData{
+    NSArray * dataSourceArr = @[@"方式1",@"方式2",@"方式3",@"方式4",@"方式5"];
+    self.dataSourceArr = dataSourceArr;
+    NSArray * descSourceArr = @[@"设置cornerRadius和masksToBounds",@"UIBezierPath和Core Graphics在图片上画出一个透明的圆",@"CAShapeLayer和UIBezierPath设置view.layer.mask",@"addsubLayer",@"两个imageView遮盖显示,切图配合提供"];
+    self.descSourceArr = descSourceArr;
+}
+#pragma mark - 创建界面
+-(void)createUI{
+    UITableView * tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-200) style:UITableViewStylePlain];
     tableView.delegate = self;
     tableView.dataSource = self;
     tableView.tableFooterView = [[UIView alloc] init];
     [self.view addSubview:tableView];
+    UILabel * descLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height-120, [UIScreen mainScreen].bounds.size.width, 30)];
+    descLabel.numberOfLines = 0;
+    [descLabel setText:@"图片大小和collectionView的Item大小一样的，避免了布局，项目中肯定需要布局"];
+    [self.view addSubview:descLabel];
 }
+#pragma mark - tableViewDelegate方法
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return self.dataSourceArr.count;
 }
