2615a5cd586065ff1210016f6c27cc9875ccdd93
qiaodaniMac <qiaodandan@QiaodabdandeiMac.local>
2017-01-09 12:26:27 +0800

    扩大button的相应范围demo

---
+++ EnlargeButtonActionArea/EnlargeButtonActionArea/EnlargedButton.h
@@ -0,0 +1,13 @@
+//
+//  EnlargedButton.h
+//  EnlargeButtonActionArea
+//
+//  Created by 乔丹丹 on 2017/1/9.
+//  Copyright © 2017年 Fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnlargedButton : UIButton
+
+@end
---
+++ EnlargeButtonActionArea/EnlargeButtonActionArea/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnlargeButtonActionArea
+//
+//  Created by 乔丹丹 on 2017/1/9.
+//  Copyright © 2017年 Fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
