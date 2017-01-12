168ebaf77b1f5f8b3dbc842f88ac00e8dfa2b59c
qiaodan <1120465896@qq.com>
2017-01-12 23:23:31 +0800

    均匀布局

---
+++ AutoLayoutTest/AutoLayoutTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutTest
+//
+//  Created by 乔丹丹 on 17/1/11.
+//  Copyright © 2017年 com.home.zxb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
