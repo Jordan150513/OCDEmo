80962026fecf9d4a5366b9fd5108e6c4e20093ee
qiaodaniMac <qiaodandan@QiaodabdandeiMac.local>
2017-01-22 13:01:38 +0800

    sd失效

--- QSDWebImageDemo/QSDWebImageDemo/ViewController.m
+++ QSDWebImageDemo/QSDWebImageDemo/ViewController.m
@@ -7,6 +7,22 @@
 //
 
 #import "ViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
+#import <SDWebImage/UIImageView+WebCache.h>
+
+
+/*
+ UIImage (GIF)中的三个方法：
+ 
+ + (UIImage *)sd_animatedGIFNamed:(NSString *)name;
+ 
+ + (UIImage *)sd_animatedGIFWithData:(NSData *)data;
+ 
+ - (UIImage *)sd_animatedImageByScalingAndCroppingToSize:(CGSize)size;
+ */
+
+static NSString * urlStringgif = @"https://img2.soufun.com/bbsv2/face/em016.gif";
+static NSString * urlStringpng = @"http://o9vi0jo2t.bkt.clouddn.com/client_uploads/images/100/803C68E5FA7D8EBE7CCE498C0D1223C8";
 
 @interface ViewController ()
 
@@ -16,7 +32,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"SDWeb Demo";
+    
+    //一般图片
+    UIImageView * imageView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 80, 80)];
+//    [imageView sd_setImageWithURL:[NSURL URLWithString:urlStringpng] placeholderImage:[UIImage imageNamed:@"placeholder"]];
+    [imageView sd_setImageWithURL:[NSURL URLWithString:urlStringpng] placeholderImage:[UIImage imageNamed:@"placeholder"] completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+        NSLog(@"done");
+    }];
+//    [imageView setImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:urlStringpng]]]];
+    [self.view addSubview:imageView];
+    
+
 }
 
 
