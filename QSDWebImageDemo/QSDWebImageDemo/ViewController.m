51438b70eee6723c266f9668258beda5b99483ff
qiaodaniMac <qiaodandan@QiaodabdandeiMac.local>
2017-01-22 13:01:23 +0800

    sd失效

---
+++ QSDWebImageDemo/QSDWebImageDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QSDWebImageDemo
+//
+//  Created by 乔丹丹 on 2017/1/22.
+//  Copyright © 2017年 Fang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ QSDWebImageDemo/QSDWebImageDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QSDWebImageDemo
+//
+//  Created by 乔丹丹 on 2017/1/22.
+//  Copyright © 2017年 Fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
