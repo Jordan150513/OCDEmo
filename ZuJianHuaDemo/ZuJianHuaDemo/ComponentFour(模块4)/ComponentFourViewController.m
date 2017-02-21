f471ae72eef9627070af4be60c75b4f0425b570e
乔丹丹 <1052691906@qq.com>
2017-02-21 14:26:01 +0800

    第三种方式 protocol-class 注册表的方式

---
+++ ZuJianHuaDemo/ZuJianHuaDemo/ComponentFour(模块4)/ComponentFourViewController.m
@@ -0,0 +1,39 @@
+//
+//  ComponentFourViewController.m
+//  ZuJianHuaDemo
+//
+//  Created by 乔丹丹 on 2017/2/21.
+//  Copyright © 2017年 Fang. All rights reserved.
+//
+
+#import "ComponentFourViewController.h"
+
+@interface ComponentFourViewController ()
+
+@end
+
+@implementation ComponentFourViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"组件 4";
+    // protocol-class 注册表的方式
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
