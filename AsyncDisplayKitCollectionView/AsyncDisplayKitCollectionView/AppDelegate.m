0ecf30c79b23023f0b54364693505073d3b25c77
乔丹丹 <1052691906@qq.com>
2017-03-07 18:33:11 +0800

    文件demo路径调整 还有一个问题 collectionView 的内容跑到后面去了？？ 什么鬼

--- AsyncDisplayKitCollectionView/AsyncDisplayKitCollectionView/AppDelegate.m
+++ AsyncDisplayKitCollectionView/AsyncDisplayKitCollectionView/AppDelegate.m
@@ -23,8 +23,8 @@
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
     self.window.backgroundColor = [UIColor whiteColor];
-    ViewController * vc = [[ViewController alloc] init];
-//    NativeViewController * vc = [[NativeViewController alloc] init];
+//    ViewController * vc = [[ViewController alloc] init];
+    NativeViewController * vc = [[NativeViewController alloc] init];
     UINavigationController * navc = [[UINavigationController alloc] initWithRootViewController:vc];
     self.window.rootViewController = navc;
     [self.window makeKeyWindow];
